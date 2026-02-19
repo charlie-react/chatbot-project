5b004977ecee514b83588f706ae775e82b9af198
Charles Afolabi <charles49ja@gmail.com>
2026-02-19 12:05:56 +0100

    Fix ASGI startup by initializing Django before importing websocket routing

--- chatbox/asgi.py
+++ chatbox/asgi.py
@@ -8,16 +8,20 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat import routing as chat_routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbox.settings")
+
+django.setup()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(chat_routing.websocket_urlpatterns)
+        URLRouter(websocket_urlpatterns)
     ),
 })
